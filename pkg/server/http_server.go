84aa709ebeb836f75699bff88758e4a090a70cf1
Lewerkun <denis@tars.co>
2021-11-15 00:29:52 +0300

    TEST

--- pkg/server/http_server.go
+++ pkg/server/http_server.go
@@ -151,14 +151,7 @@ func (h *HTTPServer) logger(handler http.Handler) http.HandlerFunc {
 	}
 }
 
-const banner = `<h1> Interactsh Server </h1>
-
-<a href='https://github.com/lewerkun/interactsh'>Interactsh</a> is an <b>open-source solution</b> for out-of-band data extraction. It is a tool designed to detect bugs that cause external interactions. These bugs include, Blind SQLi, Blind CMDi, SSRF, etc. <br><br>
-
-If you find communications or exchanges with the <b>%s</b> server in your logs, it is possible that someone has been testing your applications.<br><br>
-
-You should review the time when these interactions were initiated to identify the person responsible for this testing.
-`
+const banner = `<h1> TARS SNITCH </h1>`
 
 // defaultHandler is a handler for default collaborator requests
 func (h *HTTPServer) defaultHandler(w http.ResponseWriter, req *http.Request) {
